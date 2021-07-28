c67a429eee02604feae482d4483025fabffcb811
Nicolas Molina Monroy <hi@nicobytes.com>
2021-07-28 14:34:50 -0400

    :construction: avoid callback hell

--- src/app/pages/category/category.component.ts
+++ src/app/pages/category/category.component.ts
@@ -1,16 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { switchMap } from 'rxjs/operators';
 
 import { Product } from '../../models/product.model';
 import { ProductsService } from './../../services/products.service';
 
 @Component({
   selector: 'app-category',
-  templateUrl: './category.component.html',
-  styleUrls: ['./category.component.scss']
+  template: `<app-products
+    [products]="products"
+    (loadMore)="onLoadMore()"
+  ></app-products>`,
+  styleUrls: ['./category.component.scss'],
 })
 export class CategoryComponent implements OnInit {
-
   categoryId: string | null = null;
   limit = 10;
   offset = 0;
@@ -19,18 +22,36 @@ export class CategoryComponent implements OnInit {
   constructor(
     private route: ActivatedRoute,
     private productsService: ProductsService
-  ) { }
+  ) {}
 
   ngOnInit(): void {
-    this.route.paramMap.subscribe(params => {
-      this.categoryId = params.get('id');
-      if (this.categoryId) {
-        this.productsService.getByCategory(this.categoryId, this.limit, this.offset)
-        .subscribe(data => {
-          this.products = data;
+    this.route.paramMap
+      .pipe(
+        switchMap((params) => {
+          this.categoryId = params.get('id');
+          if (this.categoryId) {
+            return this.productsService.getByCategory(
+              this.categoryId,
+              this.limit,
+              this.offset
+            );
+          }
+          return [];
         })
-      }
-    });
+      )
+      .subscribe((data) => {
+        this.products = data;
+      });
   }
 
+  onLoadMore() {
+    if (this.categoryId) {
+      this.productsService
+        .getByCategory(this.categoryId, this.limit, this.offset)
+        .subscribe((data) => {
+          this.products = this.products.concat(data);
+          this.offset += this.limit;
+        });
+    }
+  }
 }
